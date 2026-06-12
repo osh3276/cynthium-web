a1f27343d605aad636b9d5f4c78360292808c39b
osh3276 <osh3276@gmail.com>
2026-06-12 10:51:39 -0400

    change server domain

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 		port: 5173,
 		proxy: {
 			"/api": {
-				target: "http://localhost:8000",
+				target: "https://cynthium-server.vercel.app"
 				changeOrigin: true,
 			},
 		},
